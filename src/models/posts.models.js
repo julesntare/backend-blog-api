dd7a95f7079988921ab335a14bf94bcfb09cbe63
Julesntare <julesntare@gmail.com>
2020-08-17 11:22:30 +0200

    modified Post API and implemented comment API

--- src/models/posts.models.js
+++ src/models/posts.models.js
@@ -8,7 +8,7 @@ let posts = [
 		'cover-imgUrl':
 			'https://firebasestorage.googleapis.com/v0/b/portfolio-db-d0d5c.appspot.com/o/images%2Ftimerate.PNG?alt=media&token=2fda0eb1-7f43-4e87-b2b5-1396c3d40b5e',
 		likes: 1,
-		comments: [{ 'comment-author': 'johndoe@gmail.com', 'comment-dec': 'nice post', 'replied-at': new Date() }],
+		comments: [{ author: 'johndoe@gmail.com', desc: 'nice post', 'replied-at': new Date() }],
 		state: 'published',
 		'created-at': new Date(),
 	},
@@ -19,7 +19,7 @@ let posts = [
 		'cover-imgUrl':
 			'https://firebasestorage.googleapis.com/v0/b/portfolio-db-d0d5c.appspot.com/o/images%2Ftimerate.PNG?alt=media&token=2fda0eb1-7f43-4e87-b2b5-1396c3d40b5e',
 		likes: 10,
-		comments: [{ 'comment-author': 'julentare@gmail.com', 'comment-dec': 'nice post', 'replied-at': new Date() }],
+		comments: [{ author: 'julentare@gmail.com', desc: 'nice post', 'replied-at': new Date() }],
 		state: 'published',
 		'created-at': new Date(),
 	},
