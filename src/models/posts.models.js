ce6f9509eeca8560e77971bb2042a4efba58a321
Julesntare <julesntare@gmail.com>
2020-08-26 02:45:19 +0200

    modified models

--- src/models/posts.models.js
+++ src/models/posts.models.js
@@ -2,22 +2,24 @@ import { v4 as uuidv4 } from 'uuid';
 
 let posts = [
 	{
+		id: 'c55dee33-fd60-4011-a90c-b1bfeacbe8ea',
 		author: 'Jules NTARE',
 		title: 'JS new features',
 		desc: 'lorem ipsum',
 		'cover-imgUrl': null,
 		likes: 1,
-		comments: [{ author: 'johndoe@gmail.com', desc: 'nice post', 'replied-at': new Date() }],
+		comments: [],
 		state: 'published',
 		'created-at': new Date(),
 	},
 	{
+		id: 'c55dee33-fd60-4011-a90c-b1bfeacbe8sq',
 		author: 'John Doe',
 		title: 'What is Programming',
 		desc: 'lorem ipsum',
 		'cover-imgUrl': null,
 		likes: 10,
-		comments: [{ author: 'julentare@gmail.com', desc: 'nice post', 'replied-at': new Date() }],
+		comments: [],
 		state: 'published',
 		'created-at': new Date(),
 	},
