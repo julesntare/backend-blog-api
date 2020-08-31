6bfcf007cec55156d1dec61f43540137c32c9cab
Julesntare <julesntare@gmail.com>
2020-08-31 09:25:53 +0200

    modified models

--- src/models/messages.models.js
+++ src/models/messages.models.js
@@ -1,28 +1,10 @@
-import { v4 as uuidv4 } from 'uuid';
+const mongoose = require('mongoose');
 
-let msgs = [
-	{
-		id: 'c55dee33-fd60-4011-a90c-b1bfeacbe8ea',
-		email: 'julesntare@gmail.com',
-		name: 'Jules NTARE',
-		msg: 'Hello, I need help on NodeJs',
-		date: new Date(),
-	},
-	{
-		id: 'c55dee33-fd60-4011-a90c-dfsf',
-		email: 'uwingajoselyne@gmail.com',
-		name: 'Joselyne UWINGABIRE',
-		msg: 'Try to improve performance of your site',
-		date: new Date(),
-	},
-	{
-		id: 'c55dee33-fd60-40sa-a90c-fsds',
-		email: 'bertin10@gmail.com',
-		name: 'Bertin TUYISHIME',
-		msg: 'Hello world',
-		date: new Date(),
-	},
-];
-msgs = msgs.map((msg) => ({ id: uuidv4(), ...msg }));
+const schema = mongoose.Schema({
+	name: String,
+	email: String,
+	msg: String,
+	date: { type: Date, default: new Date() },
+});
 
-export default msgs;
+module.exports = mongoose.model('Message', schema);
--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,34 +1,18 @@
-import { v4 as uuidv4 } from 'uuid';
+const mongoose = require('mongoose');
 
-let users = [
-	{
-		id: 'c55dee33-fd60-4011-a90c-b1bfeacbe8ea',
-		firstname: 'Jules',
-		lastname: 'NTARE',
-		email: 'julesntare@gmail.com',
-		'profile-img-url': null,
-		password: '4JLAj6B9pRMdhRiXhJOkRjlPMDkIxYoKrEp2j4Qgz7c=',
-		location: 'Kigali, muhima, KN88st',
-		bio: 'Software Engineer',
-		noOfEntries: 0,
-		level: 1,
-		joined: new Date(),
-	},
-	{
-		id: 'c55dee33-fd60-4011-a90c-b1bfeacbe8eq',
-		firstname: 'John',
-		lastname: 'Doe',
-		email: 'johndoe@gmail.com',
-		'profile-img-url': null,
-		password: 'dnajkdnwk',
-		location: 'London NW1 6XE',
-		bio: 'CEO',
-		noOfEntries: 0,
-		level: 2,
-		joined: new Date(),
-	},
-];
+const schema = mongoose.Schema({
+	firstname: String,
+	lastname: String,
+	email: String,
+	'profile-img-url': String,
+	password: String,
+	comments: Array,
+	state: String,
+	location: String,
+	bio: String,
+	noOfEntries: Number,
+	level: Number,
+	joined: { type: Date, default: new Date() },
+});
 
-users = users.map((user) => ({ id: uuidv4(), ...user }));
-
-export default users;
+module.exports = mongoose.model('User', schema);
