86e29eab71bfd7d6b0837818787e7d0d532cb1a8
Julesntare <julesntare@gmail.com>
2020-08-31 11:10:25 +0200

    modified invalid data status code

--- src/middlewares/validations/postValidations.js
+++ src/middlewares/validations/postValidations.js
@@ -1,31 +1,30 @@
 const mongoose = require('mongoose');
 import users from '../../models/users.models';
-import posts from '../../models/posts.models';
 
 const postAddValidations = async (req, res, next) => {
 	let userData = await users.findOne({
 		email: req.body.author,
 	});
 	if (req.body.author == undefined || req.body.title == undefined) {
-		return res.status(403).json({ msg: 'Please provide at least author and title' });
+		return res.status(400).json({ msg: 'Please provide at least author and title' });
 	}
 	if (!/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(req.body.author)) {
-		return res.status(403).json({ msg: 'author must not be empty, must be email, and have above 3 letters' });
+		return res.status(400).json({ msg: 'author must not be empty, must be email, and have above 3 letters' });
 	}
 	if (req.body.title.length < 3 || req.body.title == '' || typeof req.body.title != 'string') {
-		return res.status(403).json({ msg: 'title must not be empty, be string and have above 3 letters' });
+		return res.status(400).json({ msg: 'title must not be empty, be string and have above 3 letters' });
 	}
 	if (userData == null) {
-		return res.status(403).json({ msg: 'Author not available in system' });
+		return res.status(400).json({ msg: 'Author not available in system' });
 	}
 	if (req.body.desc) {
 		if (req.body.desc == '' || typeof req.body.desc != 'string') {
-			return res.status(403).json({ msg: 'desc must not be empty' });
+			return res.status(400).json({ msg: 'desc must not be empty' });
 		}
 	}
 	if (req.body.state) {
 		if (req.body.state != 'published' || req.body.state != 'draft' || typeof req.body.state != 'string') {
-			return res.status(403).json({ msg: 'state must be string and have to be "published" or "draft"' });
+			return res.status(400).json({ msg: 'state must be string and have to be "published" or "draft"' });
 		}
 	}
 	next();
@@ -34,22 +33,22 @@ const postAddValidations = async (req, res, next) => {
 const postEditValidations = async (req, res, next) => {
 	if (req.body.author) {
 		if (!/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(req.body.author)) {
-			return res.status(403).json({ msg: 'author must not be empty, must be email, and have above 3 letters' });
+			return res.status(400).json({ msg: 'author must not be empty, must be email, and have above 3 letters' });
 		}
 	}
 	if (req.body.title) {
 		if (req.body.title.length < 3 || req.body.title == '' || typeof req.body.title != 'string') {
-			return res.status(403).json({ msg: 'title must not be empty, be string and have above 3 letters' });
+			return res.status(400).json({ msg: 'title must not be empty, be string and have above 3 letters' });
 		}
 	}
 	if (req.body.desc) {
 		if (req.body.desc == '' || typeof req.body.desc != 'string') {
-			return res.status(403).json({ msg: 'desc must not be empty' });
+			return res.status(400).json({ msg: 'desc must not be empty' });
 		}
 	}
 	if (req.body.state) {
 		if (req.body.state != 'published' || req.body.state != 'draft' || typeof req.body.state != 'string') {
-			return res.status(403).json({ msg: 'state must be string and have to be "published" or "draft"' });
+			return res.status(400).json({ msg: 'state must be string and have to be "published" or "draft"' });
 		}
 	}
 	next();
