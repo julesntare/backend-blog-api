72f834faf1e5979f1ce8cf5d73d0db8a0d621ea4
Julesntare <julesntare@gmail.com>
2020-09-03 00:22:08 +0200

    modified middlewares

--- src/middlewares/validations/postValidations.js
+++ src/middlewares/validations/postValidations.js
@@ -23,7 +23,7 @@ const postAddValidations = async (req, res, next) => {
 		}
 	}
 	if (req.body.state) {
-		if (req.body.state != 'published' || req.body.state != 'draft' || typeof req.body.state != 'string') {
+		if (typeof req.body.state != 'string' || req.body.state != 'draft') {
 			return res.status(400).json({ msg: 'state must be string and have to be "published" or "draft"' });
 		}
 	}
@@ -31,6 +31,9 @@ const postAddValidations = async (req, res, next) => {
 };
 
 const postEditValidations = async (req, res, next) => {
+	if (Object.keys(req.body).length == 0) {
+		return res.status(409).json({ msg: 'please provide data' });
+	}
 	if (req.body.author) {
 		if (!/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(req.body.author)) {
 			return res.status(400).json({ msg: 'author must not be empty, must be email, and have above 3 letters' });
