dec3fa5cfba591856e791acfb07323fc72698284
Julesntare <julesntare@gmail.com>
2020-09-03 00:22:51 +0200

    modified routes

--- src/routes/messages.routes.js
+++ src/routes/messages.routes.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
 import { getAllMsgs, getMsgById, createMsg, deleteMsg } from '../controllers/messages.controllers';
-import verifyAuth from '../middlewares/auth';
 import adminAuth from '../middlewares/admin_auth';
 
 // get all messages
-router.get('/', verifyAuth, getAllMsgs);
+router.get('/', [adminAuth], getAllMsgs);
 
 // get specific message by ID
-router.get('/:id', verifyAuth, getMsgById);
+router.get('/:id', [adminAuth], getMsgById);
 
 // create message by ID
 router.post('/', createMsg);
 
 // delete message by ID
-router.delete('/:id', verifyAuth, deleteMsg);
+router.delete('/:id', [adminAuth], deleteMsg);
 
 module.exports = router;
